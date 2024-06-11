96f1aaeccee8dc3b2227474d03d4ae64953108f7
BuddhikaGeasman <bgeasman@gmail.com>
2024-06-11 13:23:13 -0500

    Configure Repo

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <h1>Typescript Problems</h1>
+   <ol>
+   <li>Start from the first problem and work your way down</li>
+    <li> Each problem has a description and a test</li>
+    <li> Write your solution in answer folder.</li>
+    <li> Test your solution by running the test file.</li>
+    <li> If you pass the test, move to the next problem.</li>
+    <li> File format is exercise1.ts</li>
+    <li> Test file format is exercise1.test.ts</li>
+   </ol>
+
+   <h2>Project Set Up locally</h2>
+    <ol>
+    <li>Clone the repository</li>
+    <li>Run npm install</li>
+    <li>Run npm run test</li>
+    </ol>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
